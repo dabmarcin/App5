6d9e6f210daab25da5422a52ea249561e469f370
Marcin Dabrowski <dabmarcin@gmail.com>
2024-02-20 20:08:30 +0100

    task5

--- App5/Program.cs
+++ App5/Program.cs
@@ -5,5 +5,25 @@ public class App5
     public static void Main(string[] args)
     {
         Console.WriteLine("Podaj liczbe");
+        string number = Console.ReadLine();
+
+        int[] digits = new int[10];
+
+        foreach (char c in number)
+        {
+            if(char.IsDigit(c))
+            {
+                int letter = int.Parse(c.ToString());
+                digits[letter]++;
+
+            }
+        }
+        for (int i = 0;i<10;i++)
+        {
+            if (digits[i] > 0)
+            {
+                Console.WriteLine($"Liczba cyfr{i}: {digits[i]}");
+            }
+        }
     }
 }
